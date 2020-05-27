88e92ffb72a72601076b447f3b10fb8b6c608e5f
Dima Hvatit <57565251+dimahvatit@users.noreply.github.com>
2020-05-27 17:30:27 +0300

    upload

---
+++ app.js
@@ -0,0 +1,61 @@
+$(function () {
+
+    // fixed header \\
+    let header = $("#header"),
+        intro = $("#intro");
+
+    let nav = $("#navjs");
+    let navToggle = $("#navToggle");
+
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll load resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    // smooth scroll \\
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        let elementId = $(this).data('scroll'),
+            elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("navShow");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 75
+        }, 1300);
+    });
+
+
+    // nav toggle \\
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("navShow");
+        nav.slideToggle(duration, easing, complete)
+    });
+
+
+    // reviews https://kenwheeler.github.io/slick/ \\
+
+    let slider = $("#reviewsSlider");
+
+
+
+
+
+});
